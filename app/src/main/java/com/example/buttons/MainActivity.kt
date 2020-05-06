8d1033ff1cdf6d2950fac6557f477f269bdea7ed
Fabe <fabiab.biechl@t-online.de>
2020-05-06 19:47:29 +0200

    buttons

---
+++ app/src/main/java/com/example/buttons/MainActivity.kt
@@ -0,0 +1,33 @@
+package `in`.eyehunt.androidspinner
+
+
+import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.buttons.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Create an ArrayAdapter
+        val adapter = ArrayAdapter.createFromResource(this,
+            R.array.city_list, android.R.layout.simple_spinner_item)
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        // Apply the adapter to the spinner
+        spinner.adapter = adapter
+
+    }
+
+    fun getValues(view: View) {
+        Toast.makeText(this, "Spinner 1 " + spinner.selectedItem.toString() +
+                "\nSpinner 2 " + spinner2.selectedItem.toString(), Toast.LENGTH_LONG).show()
+    }
+}
